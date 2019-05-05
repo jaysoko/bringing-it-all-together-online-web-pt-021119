563a3695c9d3d5cab40d2511367c3d74da8ba0f2
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 22:31:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,9 +51,7 @@ def self.find_by_id(id)
     SQL
   row = DB[:conn].execute(sql,id)[0]
   attr_hash = {:name => row[1], :breed => row[2], :id => row[0]}
-  dog = Dog.new(attr_hash)
-  dog.save
+  dog = Dog.create(attr_hash)
 end
-binding.pry
 
 end
