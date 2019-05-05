2e9e3ea12b5e91b0617ba34716bad09ce16fd8c2
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 21:53:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,23 @@ def initialize(attr_hash)
   @breed = attr_hash[:breed]
 end
 
+def create_table
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+
 
 end
