f6ea1eb45f32ff057c8c1f34a9f193bc55f92985
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 22:24:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ def self.find_by_id(id)
     SELECT * FROM dogs where id = ?
     SQL
   row = DB[:conn].execute(sql,id)
-  attr_hash = {:name >= row[1], :breed >= row[2], :id >= row[0]}
+  attr_hash = {:name => row[1], :breed => row[2], :id => row[0]}
   binding.pry
   dog = Dog.new(attr_hash)
   dog
