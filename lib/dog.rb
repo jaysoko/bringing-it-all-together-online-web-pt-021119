91d1108166cf807a65876d3fb6ecd01245cff96f
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 22:54:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,9 +57,11 @@ def self.find_by_id(id)
 end
 
 def self.find_or_create_by
+
 end
 
-def self.new_from_db
+def self.new_from_db(row)
+  Dog.new(id: row[0], name: row[1], breed: row[2])  
 end
 
 def self.find_by_name(name)
